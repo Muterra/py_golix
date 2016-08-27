510382f19fe37d7462806b7a024a3a4938fce036
Nick Badger <badg@nickbadger.com>
2016-08-26 18:07:32 -0700

    Updated to 0.1.1

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 
 Golix: a python library for Golix object manipulation. Create and 
 read encrypted Golix file objects.
+
+Notes to self
+-----
+
+BUILD WITH:
+python setup.py sdist
+python setup.py bdist_wheel
+
+DISTRIBUTE WITH:
+twine upload dist/<version>*
 '''
 import sys
 
@@ -34,7 +44,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='A python library for Golix object manipulation.',
     long_description=long_description,

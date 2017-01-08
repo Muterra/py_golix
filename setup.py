99bea9084eaacd5b98b26845eeb303aad2e875c3
Nick Badger <badg@nickbadger.com>
2017-01-07 16:10:24 -0800

    Partial install doesn't use smartyparse

--- setup.py
+++ setup.py
@@ -100,9 +100,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[
-        'smartyparse>=0.1.3'
-    ],
+    install_requires=[],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -110,7 +108,8 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         'full': ['donna25519>=0.1.1',
-                 'cryptography>=1.6']
+                 'cryptography>=1.6',
+                 'smartyparse>=0.1.3']
     },
 
     # If there are data files included in your packages that need to be
